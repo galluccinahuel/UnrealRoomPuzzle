c91ec4b8fbb234bf2f02380765dbd9131efd794c
galluccinahuel <galluccinahuel@gmail.com>
2022-09-27 12:30:12 -0300

    creando el jugador

--- Source/RoomPuzzle/RP_Character.cpp
+++ Source/RoomPuzzle/RP_Character.cpp
@@ -1,16 +1,23 @@
 
 
 #include "RP_Character.h"
+#include "Camera/CameraComponent.h"
 
 ARP_Character::ARP_Character()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	FPSCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FPS_CameraComponent"));
+	FPSCameraComponent->bUsePawnControlRotation = true;
+	FPSCameraComponent->SetupAttachment(RootComponent);
 
 }
 
 void ARP_Character::BeginPlay()
 {
 	Super::BeginPlay();
+
+
 	
 }
 
@@ -27,6 +34,29 @@ void ARP_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputCompon
 
 	PlayerInputComponent->BindAxis("MoveForward",this, &ARP_Character::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &ARP_Character::MoveRight);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &ARP_Character::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookRight", this, &APawn::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ARP_Character::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ARP_Character::StopJumping);
+}
+
+void ARP_Character::AddControllerPitchInput(float value)
+{
+
+	Super::AddControllerPitchInput(bIsLookInversion ? -value : value);
+	/*
+	if (bIsLookInversion)
+	{
+		Super::AddControllerPitchInput(-value);
+
+	}
+	else 
+	{
+		Super::AddControllerPitchInput(value);
+	}
+	*/
 }
 
 
@@ -39,4 +69,14 @@ void ARP_Character::MoveForward(float value)
 void ARP_Character::MoveRight(float value)
 {
 	AddMovementInput(GetActorRightVector() * value);
+}
+
+void ARP_Character::Jump()
+{
+	Super::Jump();
+}
+
+void ARP_Character::StopJumping()
+{
+	Super::StopJumping();
 }
--- Source/RoomPuzzle/RP_Character.h
+++ Source/RoomPuzzle/RP_Character.h
@@ -6,10 +6,24 @@
 #include "GameFramework/Character.h"
 #include "RP_Character.generated.h"
 
+class UCameraComponent;
+
 UCLASS()
 class ROOMPUZZLE_API ARP_Character : public ACharacter
 {
 	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCameraComponent* FPSCameraComponent;
+
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, Category ="Aiming")
+	bool bIsLookInversion;
+
 
 public:
 
@@ -23,10 +37,16 @@ protected:
 
 	void MoveRight(float value);
 
+	virtual void Jump() override;
+
+	virtual void StopJumping() override;
+
+
 public:	
 
 	virtual void Tick(float DeltaTime) override;
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	virtual void AddControllerPitchInput(float value) override;
 };
